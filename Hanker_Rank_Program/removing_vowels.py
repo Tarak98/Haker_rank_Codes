065e73c0f2abfe33d23ce683822d85a858b39503
Tarak98 <114574612+Tarak98@users.noreply.github.com>
2023-08-10 12:45:52 +0530

    secound commit

---
+++ Hanker_Rank_Program/removing_vowels.py
@@ -0,0 +1,1 @@
+print(''.join(char for char in input("Enter a string: ") if char.lower() not in 'aeiou'))
---
+++ Hanker_Rank_Program/reverse_string.py
@@ -0,0 +1,2 @@
+input_str = input("Enter a string: ")
+print("Reversed words:", " ".join(input_str.split()[::-1]))
